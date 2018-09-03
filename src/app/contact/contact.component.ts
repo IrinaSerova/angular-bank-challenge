c371f7ab956616f2cfedbcae88bfe9cc18436b26
Peter Horton <phorton@softstackfactory.org>
2018-09-03 09:37:07 +0000

    new styling added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+// import { Component, EventEmitter, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { emailValidator } from './validators/form.validator';
+
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +13,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  myForm: FormGroup;
 
-  constructor() { }
+  constructor(private router:Router) { }
+  onSubmit(){
+   alert(`this.myForm.value`);
+    this.router.navigate([`/atm`]);
+    
+  }
+  
 
 }
